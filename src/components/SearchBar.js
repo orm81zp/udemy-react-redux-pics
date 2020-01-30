1eb17e7ef1f6f1774dbcdcb7f6db34635c9090ff
Roman Onishchenko <ro@rollncode.com>
2020-01-30 10:25:43 +0200

    the end of a section 7

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
-  onInputChange(event) {
+  state = { term: '' };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <label>Image Search</label>
-          <input className="field" type="text" onChange={this.onInputChange}/>
+          <input
+            className="field"
+            value={this.state.term}
+            type="text"
+            onChange={(e) => this.setState( { term: e.target.value })}
+          />
         </form>
       </div>
     );
